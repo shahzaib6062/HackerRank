94fbcf96181e3a7e6f3e07b1d81e0a1c93f7d8be
shahzaib6062 <uafshahzaib@gmail.com>
2022-11-02 00:59:13 +0500

    day 1

---
+++ one month challange/challange 2/minMax.cpp
@@ -0,0 +1,100 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+/*
+ * Complete the 'miniMaxSum' function below.
+ *
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
+
+void miniMaxSum(vector<int> arr) {
+    long min=0, max=0 , sum=0;
+    int size=arr.size();
+    for(int i=0 ; i<size ; i++){
+        for(int j=0;j<size ; j++){
+            if(j == i){
+            }
+            else{ 
+                sum += arr[j];
+                }
+        }
+        if( i == 0){
+            max = sum;
+            min = sum;
+        }
+        else if(sum > max){
+            max = sum;
+        }
+        else if(sum < min){
+            min = sum;
+        }
+        sum = 0;
+    }
+    cout << min << " " << max;
+
+}
+
+int main()
+{
+
+    string arr_temp_temp;
+    getline(cin, arr_temp_temp);
+
+    vector<string> arr_temp = split(rtrim(arr_temp_temp));
+
+    vector<int> arr(5);
+
+    for (int i = 0; i < 5; i++) {
+        int arr_item = stoi(arr_temp[i]);
+
+        arr[i] = arr_item;
+    }
+
+    miniMaxSum(arr);
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
+
+vector<string> split(const string &str) {
+    vector<string> tokens;
+
+    string::size_type start = 0;
+    string::size_type end = 0;
+
+    while ((end = str.find(" ", start)) != string::npos) {
+        tokens.push_back(str.substr(start, end - start));
+
+        start = end + 1;
+    }
+
+    tokens.push_back(str.substr(start));
+
+    return tokens;
+}
